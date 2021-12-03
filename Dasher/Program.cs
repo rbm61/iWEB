f3a5dc047eb21dad6e1fcd5e8264907383d991a8
Raul <raulbeltmarc@gmail.com>
2021-12-03 11:11:25 +0100

    Cambio a NET 6.0

--- Dasher/Program.cs
+++ Dasher/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Dasher
 {
@@ -23,4 +24,24 @@ namespace Dasher
                     webBuilder.UseStartup<Startup>();
                 });
     }
+    public class Customer 
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Phone { get; set; }
+        public bool Admin { get; set; }
+    }
+
+    public class DasherContext : DbContext 
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+
+
+
+
 }
